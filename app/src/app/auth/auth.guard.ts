416cbd4fd36509e914fbcdb6d367dc2dd700f947
iameneiros <iameneiros@finnegans.com.ar>
2019-06-05 11:16:08 -0700

    Agrego tokenurl y que se muestre el menu o no por parametros

--- app/src/app/auth/auth.guard.ts
+++ app/src/app/auth/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from './auth.service';
+import { variable } from '@angular/compiler/src/output/output_ast';
 
 // http://jasonwatmore.com/post/2018/10/29/angular-7-user-registration-and-login-example-tutorial#app-module-ts
 
@@ -14,11 +15,23 @@ export class AuthGuard implements CanActivate {
 
     async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         const tokenByURL = route.queryParams['token'];
-        const showMenu = (route.queryParams['showmenu'] == 'true');
-        const showToolbar = (route.queryParams['showtoolbar'] == 'true');
-        console.log("Token: " + tokenByURL);
+        var showToolBar = false;
+        var showMenu = false;
+        
+        if (route.queryParams['showToolBar']==null) {
+            showToolBar = true;
+        } else {
+            showToolBar = (route.queryParams['showToolBar'] == 'true');
+        }
+        if (route.queryParams['showMenu']==null) {
+            showMenu = true;
+        } else {
+            showMenu = (route.queryParams['showMenu'] == 'true');
+        }
+        this.authenticationService.save_currentEnviroment({showMenu : Boolean(showMenu) , showToolBar : Boolean(showToolBar)});
+        
         if (tokenByURL) {
-            this.authenticationService.save_currentUser({ token: tokenByURL, showmenu: Boolean(showMenu), showtoolbar: Boolean(showToolbar) } );
+            this.authenticationService.save_currentUser({ token: tokenByURL} );
         }
         const tokenOk = await this.authenticationService.check_token();
         if (!tokenOk) {
