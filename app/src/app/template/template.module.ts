1382ead3adf0917d104327478a41c206f52fc145
Ivan <ilencina@xubio.com>
2020-01-27 13:34:34 -0300

    formulas-abm/ilencina-svc-112 Added SCSS by default for the ng generate commands.

--- app/src/app/template/template.module.ts
+++ app/src/app/template/template.module.ts
@@ -1,4 +1,3 @@
-import { FormulasContainer } from './containers/formulas/formulas.container';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
