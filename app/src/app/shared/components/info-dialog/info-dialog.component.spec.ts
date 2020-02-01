228f421e27d450b64a2cd8b092eb5c238ea073d5
Ivan <ilencina@xubio.com>
2020-02-01 01:45:25 -0300

    formulas-abm/ilencina-svc-112 MatDialogComponent tests fix.

--- app/src/app/shared/components/info-dialog/info-dialog.component.spec.ts
+++ app/src/app/shared/components/info-dialog/info-dialog.component.spec.ts
@@ -1,3 +1,6 @@
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './../../../material.module';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InfoDialogComponent } from './info-dialog.component';
@@ -8,7 +11,15 @@ describe('InfoDialogComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ InfoDialogComponent ]
+      declarations: [ InfoDialogComponent ],
+      imports: [
+        MaterialModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: { title: 'title1', text: 'text1'} }
+      ]
     })
     .compileComponents();
   }));
