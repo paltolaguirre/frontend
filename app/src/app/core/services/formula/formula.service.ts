c59469506ae8e08b32b5167df493b975429c4300
Ivan <ilencina@xubio.com>
2020-02-12 01:41:23 -0300

    feature/ilencina-svc112 Get formula categories from service.

--- app/src/app/core/services/formula/formula.service.ts
+++ app/src/app/core/services/formula/formula.service.ts
@@ -1,3 +1,4 @@
+import { FormulaCategory } from './../../models/formula-category.model';
 import { Injectable } from '@angular/core';
 import { Formula } from '../../models/formula.model';
 
@@ -34,5 +35,47 @@ export class FormulaService {
 
   public find(id: number): Formula {
     return this.formulas.find(formula => formula.id === id);
+  }
+
+  public getFormulaCategories(): FormulaCategory[] {
+    return [
+      {
+        id: 1,
+        title: 'Elementos',
+        items: [
+          {
+            id: 1,
+            img: 'assets/img/descarga.jpg',
+            title: 'Variables'
+          },
+          {
+            id: 2,
+            img: 'assets/img/descarga.jpg',
+            title: 'Conceptos en la liquidación'
+          },
+          {
+            id: 3,
+            img: 'assets/img/descarga.jpg',
+            title: 'Parámetros de entrada'
+          }
+        ]
+      },
+      {
+        id: 2,
+        title: 'Fórmulas',
+        items: [
+          {
+            id: 4,
+            img: 'assets/img/descarga.jpg',
+            title: 'Fórmulas estandar'
+          },
+          {
+            id: 5,
+            img: 'assets/img/descarga.jpg',
+            title: 'Mis fórmulas'
+          }
+        ]
+      }
+    ];
   }
 }
--- app/src/app/formula/components/formula-item-picker/formula-item-picker.component.ts
+++ app/src/app/formula/components/formula-item-picker/formula-item-picker.component.ts
@@ -1,3 +1,4 @@
+import { FormulaService } from './../../../core/services/formula/formula.service';
 import { FormulaCategory } from './../../../core/models/formula-category.model';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
@@ -12,49 +13,10 @@ export class FormulaItemPickerComponent implements OnInit {
   public isExpanded: boolean = true;
   public categories: FormulaCategory[];
 
-  constructor() {
-    this.categories = [
-      {
-        id: 1,
-        title: 'Elementos',
-        items: [
-          {
-            id: 1,
-            img: 'assets/img/descarga.jpg',
-            title: 'Variables'
-          },
-          {
-            id: 2,
-            img: 'assets/img/descarga.jpg',
-            title: 'Conceptos en la liquidación'
-          },
-          {
-            id: 3,
-            img: 'assets/img/descarga.jpg',
-            title: 'Parámetros de entrada'
-          }
-        ]
-      },
-      {
-        id: 2,
-        title: 'Fórmulas',
-        items: [
-          {
-            id: 4,
-            img: 'assets/img/descarga.jpg',
-            title: 'Fórmulas estandar'
-          },
-          {
-            id: 5,
-            img: 'assets/img/descarga.jpg',
-            title: 'Mis fórmulas'
-          }
-        ]
-      }
-    ];
-  }
+  constructor(private formulaService: FormulaService) {}
 
   ngOnInit() {
+    this.categories = this.formulaService.getFormulaCategories();
   }
 
   public onExpandClick() {
