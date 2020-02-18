205eb85732f0497fbc21592b73ac6f4f5dd93a0e
Ivan <ilencina@xubio.com>
2020-02-18 10:45:32 -0300

    feature/ilencina-svc112 Mocks created.

--- app/src/app/core/services/api-http/api-http.service.spec.ts
+++ app/src/app/core/services/api-http/api-http.service.spec.ts
@@ -1,9 +1,19 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ApiHttpService } from './api-http.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('ApiHttpService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        HttpClient
+      ]
+    });
+  });
 
   it('should be created', () => {
     const service: ApiHttpService = TestBed.get(ApiHttpService);
--- app/src/app/core/services/formula/formula.service.spec.ts
+++ app/src/app/core/services/formula/formula.service.spec.ts
@@ -1,9 +1,15 @@
+import { ApiHttpServiceMock } from './../../mocks/api-http.service.mock';
+import { ApiHttpService } from './../api-http/api-http.service';
 import { TestBed } from '@angular/core/testing';
 
 import { FormulaService } from './formula.service';
 
 describe('FormulaService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      { provide: ApiHttpService, useClass: ApiHttpServiceMock }
+    ]
+  }));
 
   it('should be created', () => {
     const service: FormulaService = TestBed.get(FormulaService);
