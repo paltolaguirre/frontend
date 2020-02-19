3ace06bbc641d670af00d642ed2497d1bc0168d9
Ivan <ilencina@xubio.com>
2020-02-19 17:29:37 -0300

    feature/ilencina-svc112 Search item added.

--- app/src/app/core/services/formula/formula.service.ts
+++ app/src/app/core/services/formula/formula.service.ts
@@ -31,43 +31,55 @@ export class FormulaService {
     return [
       {
         id: 1,
-        title: 'Elementos',
+        title: '',
         items: [
           {
             id: 1,
             img: 'assets/img/descarga.jpg',
-            title: 'Variables',
-            categoryId: 1
-          },
-          {
-            id: 2,
-            img: 'assets/img/descarga.jpg',
-            title: 'Conceptos en la liquidación',
-            categoryId: 1
-          },
-          {
-            id: 3,
-            img: 'assets/img/descarga.jpg',
-            title: 'Parámetros de entrada',
+            title: 'Buscar',
             categoryId: 1
           }
         ]
       },
       {
         id: 2,
-        title: 'Fórmulas',
+        title: 'Elementos',
         items: [
           {
-            id: 4,
+            id: 2,
             img: 'assets/img/descarga.jpg',
-            title: 'Fórmulas estandar',
+            title: 'Variables',
             categoryId: 2
           },
           {
+            id: 3,
+            img: 'assets/img/descarga.jpg',
+            title: 'Conceptos en la liquidación',
+            categoryId: 2
+          },
+          {
+            id: 4,
+            img: 'assets/img/descarga.jpg',
+            title: 'Parámetros de entrada',
+            categoryId: 2
+          }
+        ]
+      },
+      {
+        id: 3,
+        title: 'Fórmulas',
+        items: [
+          {
             id: 5,
             img: 'assets/img/descarga.jpg',
+            title: 'Fórmulas estandar',
+            categoryId: 3
+          },
+          {
+            id: 6,
+            img: 'assets/img/descarga.jpg',
             title: 'Mis fórmulas',
-            categoryId: 2
+            categoryId: 3
           }
         ]
       }
