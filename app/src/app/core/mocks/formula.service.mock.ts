b014d4984bd9a6a246657678fc53d2412184c955
Ivan <ilencina@xubio.com>
2020-02-26 11:50:08 -0300

    feature/ilencina-svc112 FormulaServiceMock fix.

--- app/src/app/core/mocks/formula.service.mock.ts
+++ app/src/app/core/mocks/formula.service.mock.ts
@@ -21,43 +21,61 @@ export class FormulaServiceMock {
     return [
       {
         id: 1,
-        title: 'Elementos',
+        title: '',
         items: [
           {
             id: 1,
             img: 'assets/img/descarga.jpg',
-            title: 'Variables',
-            categoryId: 1
-          },
-          {
-            id: 2,
-            img: 'assets/img/descarga.jpg',
-            title: 'Conceptos en la liquidación',
-            categoryId: 1
-          },
-          {
-            id: 3,
-            img: 'assets/img/descarga.jpg',
-            title: 'Parámetros de entrada',
-            categoryId: 1
+            title: 'Buscar',
+            categoryId: 1,
+            slug: 'search'
           }
         ]
       },
       {
         id: 2,
-        title: 'Fórmulas',
+        title: 'Elementos',
         items: [
+          {
+            id: 2,
+            img: 'assets/img/descarga.jpg',
+            title: 'Variables',
+            categoryId: 2,
+            slug: 'variables'
+          },
+          {
+            id: 3,
+            img: 'assets/img/descarga.jpg',
+            title: 'Conceptos en la liquidación',
+            categoryId: 2,
+            slug: 'concept'
+          },
           {
             id: 4,
             img: 'assets/img/descarga.jpg',
-            title: 'Fórmulas estandar',
-            categoryId: 2
-          },
+            title: 'Parámetros de entrada',
+            categoryId: 2,
+            slug: 'input-params'
+          }
+        ]
+      },
+      {
+        id: 3,
+        title: 'Fórmulas',
+        items: [
           {
             id: 5,
             img: 'assets/img/descarga.jpg',
+            title: 'Fórmulas estandar',
+            categoryId: 3,
+            slug: 'standard-formulas'
+          },
+          {
+            id: 6,
+            img: 'assets/img/descarga.jpg',
             title: 'Mis fórmulas',
-            categoryId: 2
+            categoryId: 3,
+            slug: 'my-formulas'
           }
         ]
       }
