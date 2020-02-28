21089cbfeb6e7178fddb2f65b56c739acf89c941
Ivan <ilencina@xubio.com>
2020-02-28 16:19:27 -0300

    feature/ilencina-svc112 Unit test: delete should call delete method from api service.

--- app/src/app/core/services/formula/formula.service.spec.ts
+++ app/src/app/core/services/formula/formula.service.spec.ts
@@ -1,3 +1,4 @@
+import { Formula } from 'src/app/core/models/formula.model';
 import { Observable, of } from 'rxjs';
 import { ApiHttpServiceMock } from './../../mocks/api-http.service.mock';
 import { ApiHttpService } from './../api-http/api-http.service';
@@ -8,6 +9,20 @@ import { FormulaService } from './formula.service';
 describe('FormulaService', () => {
   let api: ApiHttpService;
   let service: FormulaService;
+  const fakeFormula: Formula = {
+    name: 'Formula 1',
+    CreatedAt: '',
+    UpdatedAt: '',
+    DeletedAt: '',
+    params: [],
+    description: 'Esta es una formula',
+    origin: '',
+    type: '',
+    scope: '',
+    result: '',
+    value: 1,
+    valueid: 1
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -31,6 +46,16 @@ describe('FormulaService', () => {
       await service.getAll();
 
       expect(getSpy).toHaveBeenCalledWith(`${service.BASE_URL}/formulas`);
+    });
+  });
+
+  describe('delete', () => {
+    it('should call delete method from api service', async () => {
+      const getSpy = spyOn(api, 'delete').and.returnValue(of(null));
+
+      await service.delete(fakeFormula.name);
+
+      expect(getSpy).toHaveBeenCalledWith(`${service.BASE_URL}/formulas/${fakeFormula.name}`);
     });
   });
 });
