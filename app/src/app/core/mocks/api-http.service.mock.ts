d94abd1ab5e1afb5383e62ed1af62c7804851dbf
Ivan <ilencina@xubio.com>
2020-02-28 11:37:47 -0300

    feature/ilencina-svc112 ApiHttpServiceMock populated with their corresponding methods.

--- app/src/app/core/mocks/api-http.service.mock.ts
+++ app/src/app/core/mocks/api-http.service.mock.ts
@@ -1,3 +1,30 @@
+import { Observable } from 'rxjs';
 export class ApiHttpServiceMock {
-  
+  public get(endpoint: string) {
+    return new Observable((observer) => {
+      observer.complete();
+    });
+  }
+
+  public post(endpoint: string, body?: any) {
+    return new Observable((observer) => {
+      observer.complete();
+    });
+  }
+
+  public put(endpoint: string, body?: any) {
+    return new Observable((observer) => {
+      observer.complete();
+    });
+  }
+  public patch(endpoint: string, body?: any) {
+    return new Observable((observer) => {
+      observer.complete();
+    });
+  }
+  public delete(endpoint: string, body?: any) {
+    return new Observable((observer) => {
+      observer.complete();
+    });
+  }
 }
