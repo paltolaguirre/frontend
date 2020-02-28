53e6ce7b6aed45f79bb605126156c3111463a50b
Ivan <ilencina@xubio.com>
2020-02-28 11:33:07 -0300

    feature/ilencina-svc112 Added typehinting on api service methods.

--- app/src/app/core/services/api-http/api-http.service.ts
+++ app/src/app/core/services/api-http/api-http.service.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -8,23 +9,23 @@ export class ApiHttpService {
 
   constructor(private http: HttpClient) { }
 
-  public get(endpoint: string) {
+  public get(endpoint: string): Observable<object> {
     return this.http.get(endpoint);
   }
 
-  public post(endpoint: string, body?: any) {
+  public post(endpoint: string, body?: any): Observable<object> {
     return this.http.post(endpoint, body);
   }
 
-  public put(endpoint: string, body?: any) {
+  public put(endpoint: string, body?: any): Observable<object> {
     return this.http.put(endpoint, body);
 
   }
-  public patch(endpoint: string, body?: any) {
+  public patch(endpoint: string, body?: any): Observable<object> {
     return this.http.patch(endpoint, body);
 
   }
-  public delete(endpoint: string, body?: any) {
+  public delete(endpoint: string, body?: any): Observable<object> {
     return this.http.delete(endpoint, body);
   }
 }
