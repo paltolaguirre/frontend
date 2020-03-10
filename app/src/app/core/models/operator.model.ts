f109f92e9236fa924851f79a2e0d80c4731fbad5
Ivan <ilencina@xubio.com>
2020-03-10 14:06:14 -0300

    feature/ilencina-svc112 OperatorsService and OperatorModel created.

---
+++ app/src/app/core/models/operator.model.ts
@@ -0,0 +1,6 @@
+export class Operator {
+  id: number;
+  operationName: string; // 'sum'
+  type: string; // TODO: Enum without string assignament.   numeric, boolean, etc. 
+  symbol: string; // '+'
+}
