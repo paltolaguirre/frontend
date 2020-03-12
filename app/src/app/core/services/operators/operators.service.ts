13ad5780f14ff3fd5b9a544a26c9ee7304e420c4
Ivan <ilencina@xubio.com>
2020-03-12 13:05:00 -0300

    feature/ilencina-svc112 Get logical operators implemented.

--- app/src/app/core/services/operators/operators.service.ts
+++ app/src/app/core/services/operators/operators.service.ts
@@ -1,3 +1,4 @@
+import { LogicalOperatorNames } from './../../enums/logical-operator-names.enum';
 import { FormulaTransferData } from './../../models/formula-transfer-data.model';
 import { MathOperatorNames } from './../../enums/math-operator-names.enum';
 import { Operator } from './../../models/operator.model';
@@ -42,6 +43,67 @@ export class OperatorsService {
         type: MathOperatorTypes.Numeric,
         symbol: '*',
         mustRemoveFromSource: false
+      }
+    ];
+  }
+
+  public getLogicalOperators(): Operator[] {
+    return [
+      {
+        id: 1,
+        operationName: LogicalOperatorNames.If,
+        type: MathOperatorTypes.Numeric,
+        symbol: 'SI',
+        mustRemoveFromSource: false,
+        tooltip: 'Para fórmulas Condicionales. El cálculo depende del cumplimineto de una determinada condición.'
+      },
+      {
+        id: 2,
+        operationName: LogicalOperatorNames.GreaterThan,
+        type: MathOperatorTypes.Boolean,
+        symbol: '>',
+        mustRemoveFromSource: false,
+        tooltip: 'Condición: “Mayor que”'
+      },
+      {
+        id: 3,
+        operationName: LogicalOperatorNames.SmallerThan,
+        type: MathOperatorTypes.Boolean,
+        symbol: '<',
+        mustRemoveFromSource: false,
+        tooltip: 'Condición: “Menor que”'
+      },
+      {
+        id: 4,
+        operationName: LogicalOperatorNames.Equal,
+        type: MathOperatorTypes.Boolean,
+        symbol: '=',
+        mustRemoveFromSource: false,
+        tooltip: 'Condición: “Igual que”'
+      },
+      {
+        id: 5,
+        operationName: LogicalOperatorNames.Diferent,
+        type: MathOperatorTypes.Boolean,
+        symbol: '<>',
+        mustRemoveFromSource: false,
+        tooltip: 'Condición: “Diferente que”'
+      },
+      {
+        id: 6,
+        operationName: LogicalOperatorNames.And,
+        type: MathOperatorTypes.Boolean,
+        symbol: 'Y',
+        mustRemoveFromSource: false,
+        tooltip: 'Conjunción de más de una condición  “AND”'
+      },
+      {
+        id: 7,
+        operationName: LogicalOperatorNames.Or,
+        type: MathOperatorTypes.Boolean,
+        symbol: 'ó',
+        mustRemoveFromSource: false,
+        tooltip: 'Disyunción de más de una condición “OR”'
       }
     ];
   }
