c2fe7a1381bafb178b252dd3cf4c20c5ea9a75a9
Ivan <ilencina@xubio.com>
2020-03-12 13:04:34 -0300

    feature/ilencina-svc112 OperatorModel modified: optional tooltip added.

--- app/src/app/core/models/operator.model.ts
+++ app/src/app/core/models/operator.model.ts
@@ -1,11 +1,13 @@
+import { LogicalOperatorNames } from './../enums/logical-operator-names.enum';
 import { Draggable } from './../interfaces/draggable.interface';
 import { MathOperatorNames } from './../enums/math-operator-names.enum';
 import { MathOperatorTypes } from './../enums/math-operator-types.enum';
 
 export class Operator implements Draggable {
   id: number;
-  operationName: MathOperatorNames;
+  operationName: MathOperatorNames | LogicalOperatorNames;
   type: MathOperatorTypes;
   symbol: string;
   mustRemoveFromSource: boolean;
+  tooltip?: string;
 }
