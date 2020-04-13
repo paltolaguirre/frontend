c7464c5deb92eac3fffd492d88a166152c39c12d
Carlos <carlos.hector.f@gmail.com>
2020-04-13 09:02:38 -0300

    [feature/cflores-svc112]
    Se filtra grilla de formulas para mostrar solo las de tipo genericas

--- app/src/app/formula/containers/formula-list/formula-list.container.ts
+++ app/src/app/formula/containers/formula-list/formula-list.container.ts
@@ -45,7 +45,7 @@ export class FormulaListContainer implements OnInit, AfterViewInit {
 
   async ngAfterViewInit() {
     this.formulaService.formulasStore$.subscribe((formulas: Formula[]) => {
-      this.dataSource = new MatTableDataSource<Formula>(formulas);
+      this.dataSource = new MatTableDataSource<Formula>(formulas.filter(formula => formula.type == 'generic'));
       this.dataSource.paginator = this.paginator;
       this.paginator._intl.itemsPerPageLabel = 'Items por página';
       this.isLoadingResults = false;
