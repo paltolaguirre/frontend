fe95502e0ea2b5c4a20179f77e6c0390fb410add
Carlos <carlos.hector.f@gmail.com>
2020-04-24 22:32:10 -0300

    [release/cflores-svc112]
    Se muestran en el lienzo mas de una formula para permitir arrastrar de a bloques. Falta completar funcionalidad

--- app/src/app/formula/components/formula-drop-space/formula-drop-space.component.ts
+++ app/src/app/formula/components/formula-drop-space/formula-drop-space.component.ts
@@ -30,6 +30,7 @@ export class FormulaDropSpaceComponent implements OnInit, OnDestroy {
 
   public idCount: number = 0;
   public formulaResult: FormulaTerm;
+  public valuesinvoke = [];
 
   constructor(
     private formulaService: FormulaService,
@@ -61,6 +62,10 @@ export class FormulaDropSpaceComponent implements OnInit, OnDestroy {
     main.context = new this.context();
     main.context.origin = null;
     main.context.id = 1;
+
+    if(this.formulaValue && this.formulaValue !== undefined) {
+      this.valuesinvoke.push(this.formulaValue.value);
+    }
   }
 
   ngOnDestroy() {
@@ -82,18 +87,47 @@ export class FormulaDropSpaceComponent implements OnInit, OnDestroy {
 
     const data: FormulaTransferData = JSON.parse(event.dataTransfer.getData('text'));
 
-    if (data.payload.category === OperatorCategory.Logical) {
-      return this.handleLogicalOperatorClick(data);
+    if(data.payload == undefined) {
+      return;
     }
 
-    if (data.payload.category === OperatorCategory.Math) {
-      return this.handleMathOperatorClicked(data);
-    }
+    const args = [];
+    data.payload.params.forEach((param, i) => {
+      const arg = {
+          ID: 0,
+          name: param.name,
+          type: param.type,
+          valuenumber: 0,
+          valuestring: param.valuestring == undefined ? "" : param.valuestring,
+          Valueboolean: false,
+          valueinvoke: null
+      };
+      args.push(arg);
+    });
 
-    this.createDomElementWithoutChildren(data, false);
+    const formulaInvoke = {
+      ID: 0,
+      function: {
+        name: data.payload.name,
+        params: data.payload.params,
+        type: data.payload.type,
+        result: data.payload.result
+      },
+      functionname: data.payload.name,
+      args: args
+    };
+    
+    const value = {
+      ID: 0,
+      name: "",
+      type: "number",
+      valuenumber: 0,
+      valuestring: "",
+      Valueboolean: false,
+      valueinvoke: formulaInvoke
+    };
 
-    this.onDragLeave(event);
-
+    this.valuesinvoke.push(value);
     event.cancelBubble = true;
   }
 
@@ -340,7 +374,7 @@ export class FormulaDropSpaceComponent implements OnInit, OnDestroy {
   public onDragOver(event) { // allowDrop
     event.preventDefault();
 
-    this.onParamMouseOver(event);
+    //this.onParamMouseOver(event);
 
     event.cancelBubble = true;
   }
