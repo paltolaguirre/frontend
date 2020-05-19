a9f64d4ea3ea8a35f4c5dd9bc784ae4d5d2f7e2c
Ivan <ilencina@xubio.com>
2020-05-19 12:35:08 -0300

    [release/ilencina-svc112#1] Comentario agregado.

--- app/src/app/formula/components/formula-drop-space/formula-drop-space.component.ts
+++ app/src/app/formula/components/formula-drop-space/formula-drop-space.component.ts
@@ -84,6 +84,7 @@ export class FormulaDropSpaceComponent implements OnInit, OnDestroy {
   }
 
   public setDefaultArgValue(arg) {
+        // Establece la condicion por defecto de los IFS en "verdadero".
     if (arg.name === 'condicion') {
       arg.Valueboolean = true;
     }
