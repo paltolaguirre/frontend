d85f54a4b7b2cd44bb3e497f2c9c7ad024242ae6
IvanLencina <colo9311@gmail.com>
2020-05-20 20:59:15 -0300

    [release/ilencina-svc112#3] Validator applied.

--- app/src/app/shared/validators/formula-validators.ts
+++ app/src/app/shared/validators/formula-validators.ts
@@ -1,7 +1,7 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 
-export class FormulaValidator {
-  static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
+export class FormulaValidators {
+  static cannotContainSpaces(control: AbstractControl): ValidationErrors | null {
     if ((control.value as string).indexOf(' ') >= 0) {
       return { cannotContainSpace: true };
     }
