6229897a64357c426562b81e8554956c886fa169
IvanLencina <colo9311@gmail.com>
2020-05-27 20:28:18 -0300

    [release/ilencina-svc112#14] Test: getOperatorDefaultType should return the numeric type as default.

--- app/src/app/formula/components/operators-toolbar/operators-toolbar.component.spec.ts
+++ app/src/app/formula/components/operators-toolbar/operators-toolbar.component.spec.ts
@@ -7,6 +7,7 @@ import { MaterialModule } from './../../../material.module';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OperatorsToolbarComponent } from './operators-toolbar.component';
+import { MathOperatorTypes } from 'src/app/core/enums/math-operator-types.enum';
 
 describe('OperatorsToolbarComponent', () => {
   let component: OperatorsToolbarComponent;
@@ -36,6 +37,12 @@ describe('OperatorsToolbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('getOperatorDefaultType', () => {
+    it('should return the numeric type as default', () => {
+      expect(component.getOperatorDefaultType()).toEqual(MathOperatorTypes.Numeric);
+    });
   });
 
   describe('toogleMoreOperatorsVisibility', () => {
