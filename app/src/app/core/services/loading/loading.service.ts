48419f88112838a3f3fea3db9fc43448935c6230
IvanLencina <colo9311@gmail.com>
2020-06-01 20:23:22 -0300

    [feature/ilencina-svc257] LoadingService created.

---
+++ app/src/app/core/services/loading/loading.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingService {
+
+  constructor() { }
+
+  public show() {
+
+  }
+
+  public hide() {
+
+  }
+}
