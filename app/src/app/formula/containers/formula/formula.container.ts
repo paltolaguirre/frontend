67896b2b1ab8823e993048c34cd9425f5f2656ad
IvanLencina <colo9311@gmail.com>
2020-06-11 13:37:57 -0300

    [feature/ilencina-svc257] Loading added when a formula is being fetched.

--- app/src/app/formula/containers/formula/formula.container.ts
+++ app/src/app/formula/containers/formula/formula.container.ts
@@ -12,6 +12,7 @@ import { Location } from '@angular/common';
 import { FormulaTerm } from 'src/app/core/models/formula-term.model';
 import { NotificationService } from 'src/app/handler-error/notification.service';
 import { FormulaValidators } from 'src/app/shared/validators/formula-validators';
+import { LoadingService } from 'src/app/core/services/loading/loading.service';
 
 @Component({
   selector: 'app-formula',
@@ -47,7 +48,8 @@ export class FormulaContainer implements OnInit, OnDestroy {
     private formulaService: FormulaService,
     private dialog: MatDialog,
     private router: Router,
-    private notificationService: NotificationService
+    private notificationService: NotificationService,
+    private loadingService: LoadingService
   ) {
     this.buildEmptyForm();
   }
@@ -74,9 +76,13 @@ export class FormulaContainer implements OnInit, OnDestroy {
   }
 
   public async setCurrentFormula(name: string) {
+    this.loadingService.show();
+
     this.currentFormula = await this.formulaService.find(name);
 
     if (!this.currentFormula) {
+      this.loadingService.hide();
+
       return this.showNoDataDialog();
     }
 
@@ -86,6 +92,8 @@ export class FormulaContainer implements OnInit, OnDestroy {
     // TODO: Set actual formulaResult.
 
     this.buildPreLoadedForm();
+
+    this.loadingService.hide();
   }
 
   private buildEmptyForm() {
