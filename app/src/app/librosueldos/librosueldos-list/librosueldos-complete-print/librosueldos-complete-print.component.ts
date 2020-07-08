061add381d5d69b3f56faf877a217f7cf48db6f2
Walter <wschmidt@xubio.com>
2020-07-08 15:46:36 -0300

    [feature/wschmidt-svc280] Soluciono problema de rendimiento, descargar y hoja

--- app/src/app/librosueldos/librosueldos-list/librosueldos-complete-print/librosueldos-complete-print.component.ts
+++ app/src/app/librosueldos/librosueldos-list/librosueldos-complete-print/librosueldos-complete-print.component.ts
@@ -22,7 +22,8 @@ export class LibrosueldosCompletePrintComponent implements OnInit {
   public encabezado$: Observable<any> = null;
   fechadesde: any;
   fechahasta: any;
-  show: boolean;
+  showLiquidacion: boolean;
+  showEncabezado: boolean;
 
   constructor(
     private route: ActivatedRoute,
@@ -31,36 +32,36 @@ export class LibrosueldosCompletePrintComponent implements OnInit {
     public printService: PrintService,
     private notificationService: NotificationService,
     private librosueldosService: LibrosueldosService,
-  ) { }
+    ) { }
 
   ngOnInit() {
-
+    
     this.fechadesde = this.route.snapshot.queryParamMap.get('fechadesde');
     this.fechahasta = this.route.snapshot.queryParamMap.get('fechahasta');
-
-
+   
+    
     console.log("fechadesde: " + this.fechadesde);
     console.log("fechahasta: " + this.fechahasta);
 
     this.currentLiquidaciones$ = this.route.paramMap.pipe(
       switchMap(async (params: ParamMap) => {
-
+        
 
         const liquidacionesApi = await this.liquidacionService.getLiquidacionesPorFecha(null, null, 1, this.fechadesde, this.fechahasta);
-
-
+        
+        
         const liquidaciones = liquidacionesApi.items;
-        if (!this.isLegajosValidos(liquidaciones)) {
+        if(!this.isLegajosValidos(liquidaciones)) {
           const notificacion = {
             codigo: 400,
             mensaje: "Para poder realizar esta acción primero deberá completar todos los datos obligatorios de los legajos involucrados en el informe a imprimir disponibles en Sueldos>Legajos"
           }
           const ret = this.notificationService.notify(notificacion);
         } else {
-          this.show = true;
+          this.showLiquidacion = true;
         }
 
-
+        
         return liquidaciones;
       })
     );
@@ -70,24 +71,24 @@ export class LibrosueldosCompletePrintComponent implements OnInit {
 
         const datosEncabezado = await this.librosueldosService.getLibrosueldosEncabezado();
 
-        if (this.isEncabezadoValido(datosEncabezado)) {
-          this.show = true;
+        if(this.isEncabezadoValido(datosEncabezado)) {
+          this.showEncabezado = true;
         }
-
+        
         return datosEncabezado;
       })
     );
   }
 
   isEncabezadoValido(encabezado: LibrosueldosEncabezado): boolean {
-
-    if (encabezado.actividadempresa == "" || encabezado.cuitempresa == "" || encabezado.descripcion == "" || encabezado.domicilioempresa == "" || encabezado.nombreempresa == "") {
+    
+    if(encabezado.actividadempresa == "" || encabezado.cuitempresa == "" || encabezado.descripcion == "" || encabezado.domicilioempresa == "" || encabezado.nombreempresa == "") {
       const notificacion = {
         codigo: 400,
         mensaje: "Para poder realizar esta acción primero deberá completar todos los datos de la empresa"
       }
       const ret = this.notificationService.notify(notificacion);
-
+      
       return false;
     }
 
@@ -96,37 +97,37 @@ export class LibrosueldosCompletePrintComponent implements OnInit {
 
   isLegajosValidos(liquidaciones: Liquidacion[]): boolean {
     let ret = true;
-    liquidaciones.forEach(liquidacion => {
-      if (!this.isLegajoValido(liquidacion.legajo)) {
+    liquidaciones.forEach( liquidacion => {
+      if(!this.isLegajoValido(liquidacion.legajo)) {
         ret = false;
       }
     });
-
+    
     return ret;
   }
 
   // TODO: conciderar ponerlo en el servicio de Legajo
   isLegajoValido(legajo: Legajo): boolean {
-    if (!legajo.legajo) return false;
-    if (!legajo.cuil) return false;
-    if (!legajo.apellido) return false;
-    if (!legajo.nombre) return false;
-    if (!legajo.paisid) return false;
-    if (!legajo.provinciaid) return false;
-    if (!legajo.localidadid) return false;
-    if (!legajo.estadocivilid) return false;
-    if (!legajo.situacionid) return false;
-    if (!legajo.condicionid) return false;
-    if (!legajo.modalidadcontratacionid) return false;
-    if (!legajo.obrasocialid) return false;
-    if (!legajo.condicionsiniestradoid) return false;
-    if (!legajo.horasmensualesnormales) return false;
-    if (!legajo.conveniocolectivo) return false;
-    if (!legajo.categoria) return false;
-    if (!legajo.tarea) return false;
-    if (!legajo.fechaalta) return false;
+    if(!legajo.legajo) return false;
+    if(!legajo.cuil) return false;
+    if(!legajo.apellido) return false;
+    if(!legajo.nombre) return false;
+    if(!legajo.paisid) return false;
+    if(!legajo.provinciaid) return false;
+    if(!legajo.localidadid) return false;
+    if(!legajo.estadocivilid) return false;
+    if(!legajo.situacionid) return false;
+    if(!legajo.condicionid) return false;
+    if(!legajo.modalidadcontratacionid) return false;
+    if(!legajo.obrasocialid) return false;
+    if(!legajo.condicionsiniestradoid) return false;
+    if(!legajo.horasmensualesnormales) return false;
+    if(!legajo.conveniocolectivo) return false;
+    if(!legajo.categoria) return false;
+    if(!legajo.tarea) return false;
+    if(!legajo.fechaalta) return false;
 
-    return true;
+    return true; 
   }
 
   async ngAfterViewInit() {
